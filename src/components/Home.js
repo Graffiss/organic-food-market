c00a2b38a605e1543d9e192d91a3050f45d82299
Graffiss <graffis@gmail.com>
2020-03-19 11:53:07 +0100

    Routing

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div className="contents">
+				<div className="green-pics pics content">
+					<img src="./img/green-apples.jpg" alt="" />
+				</div>
+				<div className="yellow-pics pics content">
+					<img src="./img/orange-fruits.jpg" alt="" />
+				</div>
+				<div className="red-pics pics content">
+					<img src="./img/carrots.jpg" alt="" />
+				</div>
+				<div className="green-text text content">
+					<p>Go to store</p>
+					<button>
+						<a href="store.html">BUY</a>
+					</button>
+				</div>
+				<div className="yellow-text text content" />
+				<div className="red-text text content" />
+				<div className="blue-text text content" />
+				<footer />
+			</div>
+		);
+	}
+}
+
+export default Home;
---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+	<div>
+		<h2>404 - Not found</h2>
+		<button>Home</button>
+	</div>
+);
+
+export default NotFound;
