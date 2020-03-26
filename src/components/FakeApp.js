475650aed9d5698361d37b610df365fc3ebfb05d
Graffiss <graffis@gmail.com>
2020-03-26 22:14:23 +0100

    naprawione zdjecia w Store

--- src/components/FakeApp.js
+++ src/components/FakeApp.js
@@ -10,25 +10,7 @@ import NotFound from './NotFound';
 
 class FakeApp extends React.Component {
 	state = {
-		products: {
-			vegetable1: {
-				index: 1,
-				name: 'Carrot',
-				image: '../images/carrots.jpg',
-				desc: 'Everyone’s favorite white fish. We will cut it to the size you need and ship it.',
-				price: 12,
-				status: 'available'
-			},
-
-			vegetable2: {
-				index: 2,
-				name: 'Green apples',
-				image: '../images/green-apples.jpg',
-				desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
-				price: 10,
-				status: 'available'
-			}
-		},
+		products: productsDatabase,
 		order: {},
 		cartVisible: false
 	};
--- src/products-database.js
+++ src/products-database.js
@@ -1,8 +1,11 @@
+import carrotImage from './images/carrots.jpg';
+import appleImage from './images/green-apples.jpg';
+
 const products = {
 	vegetable1: {
 		index: 1,
 		name: 'Carrot',
-		image: '../images/carrots.jpg',
+		image: carrotImage,
 		desc: 'Everyone’s favorite white fish. We will cut it to the size you need and ship it.',
 		price: 12,
 		status: 'available'
@@ -11,7 +14,7 @@ const products = {
 	vegetable2: {
 		index: 2,
 		name: 'Green apples',
-		image: '../images/green-apples.jpg',
+		image: appleImage,
 		desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
 		price: 10,
 		status: 'available'
